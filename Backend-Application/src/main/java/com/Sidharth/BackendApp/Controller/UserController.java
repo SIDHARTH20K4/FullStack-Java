aeb35ab932147969e266606d564ae4a0021a1431
SIDHARTH20K4 <91970588+SIDHARTH20K4@users.noreply.github.com>
2025-10-05 13:26:17 +0530

    feat: delete user API added

--- Backend-Application/src/main/java/com/Sidharth/BackendApp/Controller/UserController.java
+++ Backend-Application/src/main/java/com/Sidharth/BackendApp/Controller/UserController.java
@@ -2,8 +2,11 @@ package com.Sidharth.BackendApp.Controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.Sidharth.BackendApp.Model.User;
@@ -27,8 +30,33 @@ public class UserController {
     }
 
     @GetMapping("/getUser/{id}")
-    User getUserByID(long id){
+    User getUserByID(@PathVariable("id") Long id){
         return userRepository.findById(id).orElseThrow(
             () -> new UserNotFoundException(id));
     }
+
+    @PutMapping("/updateUser/{id}")
+    public User updateUser(@RequestBody User newUser, @PathVariable("id") Long id) {
+        return userRepository.findById(id)
+            .map(user -> {
+                user.setUserName(newUser.getUserName());
+                user.setEmail(newUser.getEmail());
+                user.setName(newUser.getName());
+                return userRepository.save(user);
+            })
+            .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/deleteUser/{id}")
+    String deleteUser(@PathVariable Long id){
+
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+
+        User newuser = userRepository.getById(id);
+        userRepository.delete(newuser);
+        return "user deleted " +id+ " has been wiped out";
+    }
+
 }
